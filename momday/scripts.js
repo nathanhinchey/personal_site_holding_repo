030a5ff340aebc01340f091093bc60603694a7b7
Nathan Hinchey <nathanhinchey@gmail.com>
2015-05-09 13:44:59 -0400

    Push momday scripts into a separate folder

---
+++ momday/scripts.js
@@ -0,0 +1,32 @@
+window.Momday = {};
+
+window.Momday.markImagesWideOrTall = function (selector) {
+  var $el = $(selector);
+  var $images = $($el.find("img"));
+  $.each($images, function(index, image){
+    var img = new Image();
+    img.src = $(image).attr("src");
+
+    if (img.width > img.height){
+      $(image).addClass("wide");
+    } else {
+      $(image).addClass("tall");
+    }
+  });
+}
+
+window.Momday.switchPane = function (event) {
+  return function(event) {
+    var $button = $(event.currentTarget)
+    var paneClass = $button.data("pane");
+
+    var $pane = $("section." + paneClass);
+    var $active = $(".active");
+    $active.removeClass("active");
+
+    setTimeout(function () {
+      $pane.addClass("active");
+      $button.addClass("active");
+    }, 0);
+  };
+};
